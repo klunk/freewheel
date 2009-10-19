c9765413540aa412d5a5b692ea5c9a650f2723cc
Simon Wiehe <simon.wiehe@googlemail.com>
2009-10-19 21:50:06 +0000

    #2 Basic ControlServer that connects to a RemoteWorker and executes a command. There is no communication back from the RemoteWorker at this stage.

---
+++ freewheel/ControlServer/src/main/java/org/freewheelschedule/freewheel/controlserver/ControlServer.java
@@ -0,0 +1,82 @@
+package org.freewheelschedule.freewheel.controlserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import lombok.Setter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class ControlServer {
+
+	private final static Log log = LogFactory.getLog(ControlServer.class);
+
+	private @Setter int remotePort;
+	
+	public void runControlServer() {
+		log.info("Running the ControlServer ....");
+		
+		log.info("Registering the shutdown hook");
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				log.info("Shutting down the ControlServer ...");
+			}
+		});
+		
+		while (true) {
+			try {
+				log.debug("Connecting to the RemoteWorker to run a command");
+				Socket remoteWorker = new Socket("localhost", remotePort);
+				
+				PrintWriter command = new PrintWriter(remoteWorker.getOutputStream(), true);
+				BufferedReader result = new BufferedReader(new InputStreamReader(remoteWorker.getInputStream()));
+				
+				String response = result.readLine();
+				if (response.equals("HELO")) {
+					command.print("HELO /127.0.0.1\r\n");
+					command.flush();
+				} else {
+					log.error("Unexpected response from RemoteClient");
+					return;
+				}
+				response = result.readLine();
+				if (response.equals("Enter command to run")) {
+					command.print("java -version\r\n");
+					command.flush();
+				}
+				Thread.sleep(10000);
+			} catch (UnknownHostException e) {
+				log.error("Unable to open socket to RemoteWorker", e);
+				return;
+			} catch (IOException e) {
+				log.error("Unable to communicate with RemoteWorker", e);
+				return;
+			} catch (InterruptedException e) {
+				log.error("Sleep interrupted, continuing.", e);
+			}
+			
+		}
+	}
+	
+	/**
+	 * main method to start the ControlServer process.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext-ControlServer.xml");
+		
+		ControlServer server = (ControlServer)ctx.getBean("controlServer");
+		server.runControlServer();
+
+	}
+
+}
