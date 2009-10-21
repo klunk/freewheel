7a33a9ebe068edfd2e4b8ef5cbda2739f9f1a657
Simon Wiehe <simon.wiehe@googlemail.com>
2009-10-21 16:07:05 +0000

    #33 Fixed bug and added RuntimeException for a null command value.

---
+++ freewheel/Common/src/main/java/org/freewheelschedule/freewheel/common/message/JobInitiationException.java
@@ -0,0 +1,10 @@
+package org.freewheelschedule.freewheel.common.message;
+
+public class JobInitiationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
--- freewheel/Common/src/main/java/org/freewheelschedule/freewheel/common/message/JobInitiationMessage.java
+++ freewheel/Common/src/main/java/org/freewheelschedule/freewheel/common/message/JobInitiationMessage.java
@@ -4,14 +4,19 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public @ToString class JobInitiationMessage {
 
+	private final static Log log = LogFactory.getLog(JobInitiationMessage.class);
+	
 	private @Setter @Getter JobType jobType;
 	private @Setter @Getter String command;
 	private @Setter @Getter String stdout;
 	private @Setter @Getter String stderr;
 	private @Setter @Getter String serverMachine;
-	private @Setter @Getter int serverPort;
+	private @Setter @Getter Integer serverPort;
 	
 	public JobInitiationMessage() {
 		super();
@@ -25,31 +30,38 @@ public @ToString class JobInitiationMessage {
 			if (entry.contains("command=")) {
 				if (!entry.substring(entry.indexOf("=")+1).equals("null")) {
 					command = entry.substring(entry.indexOf("=")+1);
+				} else {
+					log.error("You must supply a command value");
+					throw new JobInitiationException();
 				}
 			}
 			if (entry.contains("jobType=")) {
-				if (!entry.substring(entry.indexOf("=")+1).equals("null")) {
+				if (!entry.substring(entry.indexOf("=")+1).startsWith("null")) {
 					jobType = JobType.valueOf(entry.substring(entry.indexOf("=")+1));
 				}
 			}
 			if (entry.contains("stdout=")) {
-				if (!entry.substring(entry.indexOf("=")+1).equals("null")) {
+				if (!entry.substring(entry.indexOf("=")+1).startsWith("null")) {
 					stdout = entry.substring(entry.indexOf("=")+1);
 				}
 			}
 			if (entry.contains("stderror=")) {
-				if (!entry.substring(entry.indexOf("=")+1).equals("null")) {
+				if (!entry.substring(entry.indexOf("=")+1).startsWith("null")) {
 					stderr = entry.substring(entry.indexOf("=")+1);
 				}
 			}
 			if (entry.contains("serverMachine=")) {
-				if (!entry.substring(entry.indexOf("=")+1).equals("null")) {
+				if (!entry.substring(entry.indexOf("=")+1).startsWith("null")) {
 					serverMachine = entry.substring(entry.indexOf("=")+1);
 				}
 			}
 			if (entry.contains("serverPort=")) {
-				if (!entry.substring(entry.indexOf("=")+1).equals("null")) {
-					serverPort = Integer.valueOf(entry.substring(entry.indexOf("=")+1));
+				if (!entry.substring(entry.indexOf("=")+1).startsWith("null")) {
+					if (entry.endsWith(")")) {
+						serverPort = Integer.valueOf(entry.substring(entry.indexOf("=")+1,entry.length()-1));
+					} else {
+						serverPort = Integer.valueOf(entry.substring(entry.indexOf("=")+1));
+					}
 				}
 			}
 		}
